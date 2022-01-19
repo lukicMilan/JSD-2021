1bd6400363ce05b99b6da7275c12f81e7da23d3e
lukicMilan <lukic.milan22@gmail.com>
2022-01-19 23:46:42 +0100

    Implement creation of model from metamodel

---
+++ src/classes/document.py
@@ -0,0 +1,25 @@
+class Document:
+
+    def __init__(self):
+        self.title = ""
+        self.paragraph = []
+
+    def __str__(self):
+        returnStr = ""
+
+        returnStr += f"We have title: {self.title}\n"
+        returnStr += f"And paragraph: \n"
+
+        i = 0
+        for p in self.paragraph:
+            returnStr += f"[{i}] {p}\n\n"
+            i+=1
+
+        return returnStr
+
+    def interpreter(self, model):
+
+        self.title = model.title
+
+        for p in model.paragraph:
+            self.paragraph.append(p)
