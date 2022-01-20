b916291faa8cb2524e2ed11e2eae0d0fe89740b7
milanatucakov <57504983+milanatucakov@users.noreply.github.com>
2022-01-21 00:42:10 +0100

    Add converters

--- src/classes/document.py
+++ src/classes/document.py
@@ -1,25 +1,32 @@
 class Document:
 
-    def __init__(self):
-        self.title = ""
-        self.paragraph = []
+  def __init__(self):
+    self.title = ""
+    self.paragraph = []
 
-    def __str__(self):
-        returnStr = ""
+  def __str__(self):
+    returnStr = ""
 
-        returnStr += f"We have title: {self.title}\n"
-        returnStr += f"And paragraph: \n"
+    returnStr += f"We have title: {self.title}\n"
+    returnStr += f"And paragraph: \n"
 
-        i = 0
-        for p in self.paragraph:
-            returnStr += f"[{i}] {p}\n\n"
-            i+=1
+    i = 0
+    for p in self.paragraph:
+      returnStr += f"[{i}] {p}\n\n"
+      i+=1
 
-        return returnStr
+    return returnStr
 
-    def interpreter(self, model):
+  def interpreter(self, model):
 
-        self.title = model.title
+    self.title = model.title
 
-        for p in model.paragraph:
-            self.paragraph.append(p)
+    for p in model.paragraph:
+      self.paragraph.append(p)
+
+  def get_dict(self):
+    
+    return {
+      "title": self.title,
+      "paragraph": self.paragraph
+    }
--- src/main.py
+++ src/main.py
@@ -1,13 +1,30 @@
 from textx import metamodel_from_file
 from classes.document import Document
+from generator.document2html import document2html
+from generator.html2pdf import html2pdf
 
 def main():
-    metamodel = metamodel_from_file("metamodel/grammar.tx")
-    model = metamodel.model_from_file("../testfile.tff")
+  metamodel = metamodel_from_file("metamodel/grammar.tx")
+  model = metamodel.model_from_file("../testfile.tff")
+    
+  document = Document()
+  document.interpreter(model)
+    
+  # TODO: Fill options from document
+  options = {
+    'page-size': 'Letter',
+    'margin-top': '0.35in',
+    'margin-right': '0.75in',
+    'margin-bottom': '0.75in',
+    'margin-left': '0.75in',
+    'encoding': "UTF-8",
+    'no-outline': None,
+    'enable-local-file-access': None
+  }
 
-    document = Document()
-    document.interpreter(model)
-    print(document)
+  generated_html = "generated.html"
+  document2html(document.get_dict(), 'template.html', generated_html)
+  html2pdf(generated_html, "generated.pdf", options)
 
 if __name__ == "__main__":
-    main()
+  main()
